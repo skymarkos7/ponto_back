55d86b4ca7abfbef2edc9a79a07a5ff23edb15cd
marcos <marcos@slice.global>
2023-04-28 12:35:08 -0300

    add link registro

---
+++ app/Http/Controllers/RegisterControler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterControler extends Controller
+{
+    public function register(){
+        return "Register";
+    }
+
+}
